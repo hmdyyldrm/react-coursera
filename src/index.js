7806667ac9148f25b6911360d1860b7273bf373c
hamdi <hamdiyyildirim@gmail.com>
2022-12-07 21:53:47 +0100

    Header and Footer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import 'bootstrap-social/bootstrap-social.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
